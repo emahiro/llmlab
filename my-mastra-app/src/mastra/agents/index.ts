56392d5c8098a7c29a22c1b906f4448e59e3612c
emahiro <ema.hiro@gmail.com>
2025-04-06 23:36:17 +0900

    add: mcp client on mastra

--- my-mastra-app/src/mastra/agents/index.ts
+++ my-mastra-app/src/mastra/agents/index.ts
@@ -1,6 +1,7 @@
 import { google } from '@ai-sdk/google';
 import { Agent } from '@mastra/core/agent';
 import { hogeTools, weatherTool } from '../tools';
+import { MastraMCPClient } from "@mastra/mcp";
 
 export const weatherAgent = new Agent({
   name: 'お天気アシスタント',
@@ -31,3 +32,55 @@ export const HogeAgent = new Agent({
   tools: { hogeTools },
 });
 
+// MCP Client Example
+export const sampleMCPAgent = new Agent({
+  name: "sample MCP Agent",
+  instructions: `
+  - You are able to fetch data from URLs on demand and discuss the response data with the user.
+  - If the location name isn’t in English, please translate it
+  // 質問者の言語に合わせて回答する言語を選択すること（ex 日本語で聞かれたら日本語で答える。）
+  - Always respond in the language of the question.
+  `,
+  model: google('gemini-1.5-pro-latest'),
+});
+
+// Initialize the MCP client using mcp/fetch as an example https://hub.docker.com/r/mcp/fetch
+// Visit https://github.com/docker/mcp-servers for other reference docker mcp servers
+const mcpClient = new MastraMCPClient({
+  name: "mcp-fetch-sample",
+  server: {
+    command: "podman",
+    args: [
+      "run",
+      "-i",
+      "--rm",
+      "mcp/fetch:latest",
+    ],
+  },
+});
+
+try {
+  // Connect to the MCP server
+  await mcpClient.connect();
+  // Gracefully handle process exits so the docker subprocess is cleaned up
+  process.on("exit", () => {
+    mcpClient.disconnect();
+  });
+  // Get available tools
+  const tools = await mcpClient.tools();
+  // Use the agent with the MCP tools
+  const response = await sampleMCPAgent.generate(
+    "Tell me about mastra.ai/docs. Tell me generally what this page is and the content it includes.",
+    {
+      toolsets: {
+        fetch: tools,
+      },
+    },
+  );
+  console.log(`\n\n ${response.text}`);
+} catch (error) {
+  console.error("Error:", error);
+} finally {
+  // Always disconnect when done
+  await mcpClient.disconnect();
+}
--- my-mastra-app/src/mastra/index.ts
+++ my-mastra-app/src/mastra/index.ts
@@ -2,10 +2,10 @@
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 
-import { HogeAgent, weatherAgent } from './agents';
+import { HogeAgent, sampleMCPAgent, weatherAgent } from './agents';
 
 export const mastra = new Mastra({
-  agents: { weatherAgent, HogeAgent },
+  agents: { weatherAgent, HogeAgent, sampleMCPAgent },
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
